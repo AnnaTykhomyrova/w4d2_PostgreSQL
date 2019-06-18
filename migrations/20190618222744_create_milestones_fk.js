186ed0da8d2312046da7d706bcbb576f91d332eb
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-06-18 22:45:11 +0000

    fix second migration

--- migrations/20190618222744_create_milestones_fk.js
+++ migrations/20190618222744_create_milestones_fk.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
       table.integer('famous_person_id').unsigned();
       table.foreign('famous_person_id').references('famous_people.id');
     })
-  ])
+  ]);
 };
 
 exports.down = function(knex, Promise) {
